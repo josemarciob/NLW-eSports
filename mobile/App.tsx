a376bd9da2e503020a6f0fb66dab05ad5d1ffa38
José Marcio <jose.souza.24716@a.fecaf.com.br>
2022-09-18 21:25:09 -0300

    Utilizando o sistema de notificações do expo-notifications, pegando o token de acesso para enviar.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { StatusBar } from 'react-native';
 
 import {
@@ -8,11 +9,20 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 
+import {Subscription} from 'expo-modules-core'
+
+import * as Notifications from 'expo-notifications';
+import './src/services/notificationConfigs'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
+
+
 import { Routes } from './src/routes';
 
 import { Background } from './src/components/Background';
-
 import { Loading } from './src/components/Loading';
+
+
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,6 +31,32 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+
+    
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  })
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => { 
+      console.log(response)
+    })
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  },[])
+
   return (
     <Background >
       <StatusBar 
